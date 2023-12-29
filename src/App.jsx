4a89ae53b1c6767e18f01f251d4c13996f9846e7
PrincePandeycode <pandeyprince4723@gmail.com>
2023-12-29 16:13:33 +0530

    changing states based on conditions

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ function App() {
     
    
       <div className="w-full h-screen bg-zinc-900 text-white p-5">
-  
+          1. Changing state
           <h1>{a}</h1>
           <button onClick={()=>b(a+1) } className='py-4 px-2 m-5 bg-green-600 rounded-md'>click to change</button>
 
           <Products naam = "Prince" data ={{age:19,college:"msi"}}/>
+
+
       </div>
   
   )
--- src/Products.jsx
+++ src/Products.jsx
@@ -2,13 +2,27 @@ import React, { useState } from 'react'
 
 function Products({naam, data}) {
      var [a,b] = useState(67);
+     const [c,d] = useState(false);
   return (
+    <>
     <div className="w-full  bg-zinc-700 text-white p-5">
+       2. Passing Props
         <h1>{naam}</h1>
 
         <h1>{data.age}</h1>
         <h1>{data.college}</h1>
-       </div>
+
+</div>
+   <div>   
+    3. changing state and style based on condition
+   <h1 className={`${"bg-blue-100"} ${c === false ? "text-red-600 p-5":"text-blue-600 p-5"}`}> {c === false ? "value is flase" : "The value is true"}</h1>
+        
+ 
+        <button className='bg-green-700 px-5 py-3 rounded md'  onClick={()=>d(!c)}>change val</button>
+        </div>
+        
+    
+    </>
 
   )
 }
